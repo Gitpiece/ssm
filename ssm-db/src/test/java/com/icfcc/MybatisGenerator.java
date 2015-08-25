078a384f32b09afb86b885a34ae49ecc3b5fea38
WangHuanyu <why.DK37@gmail.com>
2015-08-25 17:14:53 +0800

    把项目划分为子项目

--- ssm-db/src/test/java/com/icfcc/MybatisGenerator.java
+++ ssm-db/src/test/java/com/icfcc/MybatisGenerator.java
@@ -1,8 +1,6 @@
-package com.icfcc.db;
+package com.icfcc;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by admin on 2015/8/16.
@@ -11,11 +9,11 @@ public class MybatisGenerator {
     public static final String resourcePath = System.getProperty("user.dir")
             + File.separator + "src\\test\\resources" + File.separator;
     public static void main(String[] args) {
-        System.out.println(new File(resourcePath,"generatorConfig.xml").getAbsolutePath());
+        System.out.println(new File(resourcePath, "generatorConfig.xml").getAbsolutePath());
         int i = 0;
         args = new String[4];
         args[i++] = "-configfile";
-        args[i++] = new File(resourcePath,"generatorConfig.xml").getAbsolutePath();
+        args[i++] = new File(resourcePath, "generatorConfig.xml").getAbsolutePath();
         args[i++] = "-overwrite";
         args[i++] = "-verbose";
 //        args[i++] = "-tables";
